b43a0b9ff65c81336a063acfa2aae65be76bfa38
Caolan McMahon <caolan@caolanmcmahon.com>
2010-11-14 13:29:27 +0000

    add listing of blobs below a path for a given tag, branch or commit

--- test/test-git.js
+++ test/test-git.js
@@ -31,6 +31,48 @@ module.exports = nodeunit.testCase({
             ]);
             test.done();
         });
+    },
+
+    'parse blobs from ls-tree output': function (test) {
+        test.expect(3);
+        git.cmd = function (wd, commands, callback) {
+            test.equal(wd, 'working_dir');
+            test.same(commands, [
+                'ls-tree', '-r', '--full-name', 'treeish', 'path'
+            ]);
+            callback(
+                null,
+                '100644 blob c62f4c77b61f72c9632a7c208464488e9c59324d' +
+                    '\tsrc/dateinput/dateinput.js\n' +
+                '100644 blob b51e867bee540ae271aa7f495957e0f2f5ea60b8' +
+                    '\tsrc/overlay/overlay.apple.js\n' +
+                '100644 blob 163cb4bd101eb1a187e23b8745e43ec77022636e' +
+                    '\tsrc/overlay/overlay.js\n'
+            );
+        };
+        git.lsBlobs('working_dir', 'treeish', 'path', function (err, blobs) {
+            test.same(blobs, [
+                {
+                    mode: '100644',
+                    type: 'blob',
+                    hash: 'c62f4c77b61f72c9632a7c208464488e9c59324d',
+                    path: 'src/dateinput/dateinput.js'
+                },
+                {
+                    mode: '100644',
+                    type: 'blob',
+                    hash: 'b51e867bee540ae271aa7f495957e0f2f5ea60b8',
+                    path: 'src/overlay/overlay.apple.js'
+                },
+                {
+                    mode: '100644',
+                    type: 'blob',
+                    hash: '163cb4bd101eb1a187e23b8745e43ec77022636e',
+                    path: 'src/overlay/overlay.js'
+                }
+            ]);
+            test.done();
+        });
     }
 
 });
